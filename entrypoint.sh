78a738ae0885872382ede2e374387f3d43b95654
agiwalpooja20 <agiwalpooja20@gmail.com>
2020-04-23 16:31:18 +0530

    Terratest github action

---
+++ entrypoint.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+set -eo pipefail
+
+GODIR=/go/src/github.com/fac
+
+# Setup SSH KEY for private repo access
+echo "Setting up SSH key"
+mkdir -p /root/.ssh && chmod 700 /root/.ssh
+echo "${INPUT_SSH_PRIV_KEY}" > /root/.ssh/id_rsa
+unset INPUT_SSH_PRIV_KEY
+
+chmod 600 /root/.ssh/id_rsa
+eval $(ssh-agent -s)
+ssh-add
+
+# Pre-seed host keys for github.com
+ssh-keyscan github.com >> /root/.ssh/known_hosts
+
+# Allow terraform version override
+if [[ ! -z "$INPUT_TERRAFORM_VERSION" ]]; then
+  echo "terraform_version override set to $INPUT_TERRAFORM_VERSION"
+  echo "$INPUT_TERRAFORM_VERSION" > .terraform-version
+  tfenv install "$INPUT_TERRAFORM_VERSION"
+else
+  # Make sure we have the correct terraform version, if we have a .terraform-version file
+  tfenv install || true
+fi
+
+# Setup under GOPATH so dep etc works
+echo "Setting up GOPATH to include $PWD"
+CHECKOUT=$(basename "$PWD")
+mkdir -p $GODIR
+ln -s "$(pwd)" "${GODIR}/${CHECKOUT}"
+
+cd "${GODIR}/${CHECKOUT}/test"
+echo "Running go dep"
+dep init
+
+echo "Starting tests"
+gotestsum --format standard-verbose -- -v -timeout 50m -parallel 128
